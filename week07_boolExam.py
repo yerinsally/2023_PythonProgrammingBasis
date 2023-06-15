18007406cc66b55930a10141c14aac59c6b106a2
Yerin Jung <115604574+yerinsally@users.noreply.github.com>
2023-06-15 21:20:43 +0900

    Add files via upload

---
+++ week07_boolExam.py
@@ -0,0 +1,26 @@
+# and : 논리곱, or : 논리합, & : 논리곱(비트연산자), | : 논리합 (비트연산자)
+
+print(True or True, True or False, False or True, False or False)
+print(True | True, True | False, False | True, False | False)
+print(True and True, True and False, False and True, False and False)
+print(True & True, True & False, False & True, False & False)
+
+'''print(not True, not False)
+print(int(True), int(False), float(True), float(False))
+
+print(bool(0), bool(1), bool(-3.14))
+print(bool(""), bool(" "), bool("False"))
+
+print(False + False, True + False, True + True, False * True, True * True)
+
+x = True or False and True   #and 연산후 or
+y = True or True and False   #and 연산후 or
+print("x =",x,"y =",y)
+
+x = (True or False) and True
+y = (True or True) and False
+print("x =",x,"y =",y)
+
+print(not False & False)  #1. False & False 2.not
+
+print(True and (not True)) #1. (  )  2.True and'''
