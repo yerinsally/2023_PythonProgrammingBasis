210b0911befd9be702279816bd01ec2c7cd85408
Yerin Jung <115604574+yerinsally@users.noreply.github.com>
2023-06-17 15:06:44 +0900

    Add files via upload

---
+++ week10_mod2.py
@@ -0,0 +1,11 @@
+# mod2.py
+# 믈래스, 변수도 포함하는 모듈
+
+PI = 3.141592   # PI 변수
+
+class Math:     # Math 클래스
+    def solv(self, r):
+        return PI * (r**2)
+
+def add(a,b):   # add 함수
+    return a+b
