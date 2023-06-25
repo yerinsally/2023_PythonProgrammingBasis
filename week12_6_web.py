a050f7b5e3a2a5e936605bd83a8ebb1d2e18f2aa
Yerin Jung <115604574+yerinsally@users.noreply.github.com>
2023-06-25 18:27:26 +0900

    Add files via upload

---
+++ week12_6_web.py
@@ -0,0 +1,5 @@
+# webbrowser
+
+import webbrowser
+webbrowser.open('https://google.com')
+webbrowser.open_new('https://google.com')
---
+++ week12_math.py
@@ -0,0 +1,7 @@
+# math.gcd() : 최대공약수
+import math
+print(math.gcd(60,100,80))
+print(60/20,100/20,80/20)   # 한 봉지에 담을 개수
+
+# math.lcm() : 최소공배수
+print(math.lcm(15,25))
